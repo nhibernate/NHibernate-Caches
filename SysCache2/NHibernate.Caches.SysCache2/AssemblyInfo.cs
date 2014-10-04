23e5d43ad3c9dfb233f6077ab496b7c0f43025e9
Ricardo Peres <rjperes@hotmail.com>
2014-10-04 08:46:44 +0100

    Added SysCache2 Copied from f7e52337f50ac8c54e9d43ba3ef7009c0f47435c

--- SysCache2/NHibernate.Caches.SysCache2/AssemblyInfo.cs
+++ SysCache2/NHibernate.Caches.SysCache2/AssemblyInfo.cs
@@ -7,9 +7,8 @@ using System.Reflection;
 [assembly: AssemblyCompanyAttribute("NHibernate.org")]
 [assembly: AssemblyProductAttribute("NHibernate.Caches.SysCache2")]
 [assembly: AssemblyCopyrightAttribute("Licensed under LGPL.")]
-[assembly: AssemblyVersionAttribute("3.1.0.4000")]
-[assembly: AssemblyInformationalVersionAttribute("3.1.0.4000")]
-[assembly: AssemblyFileVersionAttribute("3.1.0.4000")]
+[assembly: AssemblyVersionAttribute("4.0.0.4000")]
+[assembly: AssemblyInformationalVersionAttribute("4.0.0.4000")]
+[assembly: AssemblyFileVersionAttribute("4.0.0.4000")]
 //[assembly: AssemblyKeyFileAttribute("../../src/NHibernate.snk")]
 //[assembly: AssemblyDelaySignAttribute(false)]
-
