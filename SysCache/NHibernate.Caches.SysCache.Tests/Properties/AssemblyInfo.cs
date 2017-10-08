a57c174e871a523ca26bd75dbd881fddf9a2f1f7
Frédéric Delaporte <fredericdelaporte@free.fr>
2017-10-08 17:34:23 +0200

    NHCH-49 - Centralizes version and increments it.

--- SysCache/NHibernate.Caches.SysCache.Tests/Properties/AssemblyInfo.cs
+++ SysCache/NHibernate.Caches.SysCache.Tests/Properties/AssemblyInfo.cs
@@ -1,7 +1,3 @@
 using System;
-using System.Reflection;
 
 [assembly: CLSCompliant(false)]
-[assembly: AssemblyVersion("4.0.1.4000")]
-[assembly: AssemblyInformationalVersion("4.0.1.4000")]
-[assembly: AssemblyFileVersion("4.0.1.4000")]
