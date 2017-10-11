91a8612906a8c24c590b742460f6fc1be79c4a4d
Frédéric Delaporte <fredericdelaporte@free.fr>
2017-10-11 18:45:10 +0200

    Fix NuGet package generation.

--- nugetdeploy.bat
+++
@@ -1,5 +0,0 @@
-nuget pack SysCache\NHibernate.Caches.SysCache\NHibernate.Caches.SysCache.nuspec
-nuget push -source http://packages.nuget.org/v1/ NHibernate.Caches.SysCache.5.0.0.nupkg %1
-
-nuget pack SysCache2\NHibernate.Caches.SysCache2\NHibernate.Caches.SysCache2.nuspec
-nuget push -source http://packages.nuget.org/v1/ NHibernate.Caches.SysCache2.5.0.0.nupkg %1
